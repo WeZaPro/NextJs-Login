260c30988bfa563666d3d846aecfa4a233ea5c32
Imac <imac@Imacs-iMac.local>
2022-12-18 12:53:18 +0700

    first commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+require("dotenv").config();
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        name: { label: "Name", type: "text" },
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+        password1: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.URL_API}+/signup`, {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        console.log("date---> ", data);
+
+        if (data.success == true) {
+          return data.user;
+        }
+        return null;
+      },
+    }),
+  ],
+});
